8a18b777a9cf7e3ddc369555a1e3023a8fecdae9
zeynepfahriye <fahriyezeynepcakir@gmail.com>
2024-08-06 00:03:32 +0300

    tabs

--- frontend/e-commerce/src/components/Reviews/Reviews.jsx
+++ frontend/e-commerce/src/components/Reviews/Reviews.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import "./Reviews.css"
 import ReviewsItem from './ReviewsItem'
 import ReviewsForm from './ReviewsForm'
-const Reviews = () => {
+const Reviews = ({active}) => {
    return (
-      <div className="tab-panel-reviews">
+      <div className={`tab-panel-reviews ${active}`}>
          <h3>2 reviews for Basic Colored Sweatpants With Elastic Hems</h3>
          <div className="comments">
             <ol className="comment-list">
