7789fbbb5c85a91d3274ce3f74704d1708c0e8fa
zeynepfahriye <fahriyezeynepcakir@gmail.com>
2024-08-05 17:16:12 +0300

    modal dialog

--- frontend/e-commerce/src/layouts/MainLayout.jsx
+++ frontend/e-commerce/src/layouts/MainLayout.jsx
@@ -3,12 +3,15 @@ import Header from '../components/Layout/Header/Header'
 import Footer from '../components/Layout/Footer/Footer'
 import PropTypes from "prop-types"
 import Search from '../components/Modals/Searh/Search'
+import Dialog from '../components/Modals/Dialog/Dialog'
 const MainLayout = ({ children }) => {
    const [isSearchShow, setIsSearchShow] = useState(false)
+   const [isDialogShow, setIsDialogShow] = useState(true)
    return (
       <>
-         <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow}/>
-         <Header setIsSearchShow = {setIsSearchShow} />
+         <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
+         <Dialog isDialogShow={isDialogShow} setIsDialogShow={setIsDialogShow}/>
+         <Header setIsSearchShow={setIsSearchShow} />
          {children}
          <Footer />
       </>
