2cdeca0bd158646402155d50fe0334b4799d9421
zeynepfahriye <fahriyezeynepcakir@gmail.com>
2024-08-09 02:45:27 +0300

    mongodb bağlantısı

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+const port = 5000;
 
+dotenv.config();
 
-app.get("/",(req,res)=>{
+const connect = async () => {
+   try {
+      await mongoose.connect(process.env.MONGO_URI);
+      console.log("connect to mongodb")
+   } catch (error) {
+      throw error
+   }
+}
+
+app.get("/", (req, res) => {
    res.send("Hello express js");
 })
 
-app.listen(5000,()=>{
-   console.log(`sunucu ${5000} portunda çalışıyor`)
+app.listen(port, () => {
+   connect()
+   console.log(`sunucu ${port} portunda çalışıyor`)
 })
 
