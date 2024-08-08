0f256b863776b1ad6dc4fd5dfc74b2933c7afd3c
zeynepfahriye <fahriyezeynepcakir@gmail.com>
2024-08-08 21:16:49 +0300

    react router dom

--- frontend/e-commerce/src/App.jsx
+++ frontend/e-commerce/src/App.jsx
@@ -1,15 +1,24 @@
 
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import ProductsDetailPage from './pages/ProductsDetailPage';
+import ShopPage from './pages/ShopPage';
+import BlogPage from './pages/BlogPage';
+import ContactPage from './pages/ContactPage';
 import CartPage from './pages/CartPage';
+import AuthPage from './pages/AuthPage';
+import './App.css'
 
 function App() {
 
   return (
-    <>
-      <CartPage />
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/shop' element={<ShopPage />} />
+      <Route path='/blog' element={<BlogPage />} />
+      <Route path='/contact' element={<ContactPage />} />
+      <Route path='/cart' element={<CartPage />} />
+      <Route path='/auth' element={<AuthPage />} />
+    </Routes>
   )
 }
 
--- frontend/e-commerce/src/main.jsx
+++ frontend/e-commerce/src/main.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
 import MainLayout from './layouts/MainLayout.jsx'
+import CardProvider from './context/CardProvider.jsx';
+import App from './App.jsx'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './index.css'
-import CardProvider from './context/CardProvider.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <CardProvider>
       <MainLayout>
         <App />
       </MainLayout>
     </CardProvider>
 
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
