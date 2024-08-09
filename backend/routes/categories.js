9affc25d3bbd3543eff5b2e012e110920cb0a060
zeynepfahriye <fahriyezeynepcakir@gmail.com>
2024-08-09 18:18:52 +0300

    read-single category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -14,7 +14,7 @@ router.post("/", async (req, res) => {
       console.log(error)
    }
 })
-//tüm kategorileri getirme
+//tüm kategorileri getirme (read)
 router.get("/", async (req, res) => {
    try {
       const categories = await Category.find();
@@ -24,4 +24,21 @@ router.get("/", async (req, res) => {
    }
 })
 
+//belirli bir kateroriyi getirme (read single)
+router.get("/:categoryId", async (req, res) => {
+   try {
+      const categoryId = req.params.categoryId
+      try {
+         const category = await Category.findById(categoryId)
+         res.status(200).json(category)
+      } catch (error) {
+         console.log(error)
+         res.status(404).json({ error: "category not found" })
+      }
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "server error" })
+   }
+
+})
 module.exports = router;
