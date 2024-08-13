decae5606b3f4fb2e79bb3e2dd0355640651a3ac
zeynepfahriye <fahriyezeynepcakir@gmail.com>
2024-08-14 00:36:05 +0300

    register

--- frontend/e-commerce/src/components/Auth/Register.jsx
+++ frontend/e-commerce/src/components/Auth/Register.jsx
@@ -1,26 +1,68 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { message } from "antd"
+import { useNavigate } from 'react-router-dom';
 const Register = () => {
+
+   const navigate = useNavigate()
+   const [form, setForm] = useState({
+      username: "",
+      email: "",
+      password: ""
+   });
+
+   const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setForm({
+         ...form,
+         [name]: value,
+      })
+   }
+
+   const handleRegister = async (e) => {
+      e.preventDefault()
+      try {
+         const response = await fetch(`http://localhost:5000/api/auth/register`, {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form)
+         });
+         if (response.ok) {
+            const data = await response.json();
+            //jwt token kullanılmalı
+            localStorage.setItem("user", JSON.stringify(data));
+            message.success("Kayıt Başarılı")
+            navigate("/")
+         } else {
+            message.error("Kayıt başarısız")
+         }
+         console.log(response)
+      } catch (error) {
+         console.log("giriş hatası", error)
+      }
+   }
+
    return (
       <div className="account-column">
          <h2>Register</h2>
-         <form>
+         <form onSubmit={handleRegister}>
             <div>
                <label>
                   <span>Username <span className="required">*</span></span>
-                  <input type="text" />
+                  <input type="text" onChange={handleInputChange} name='username' />
                </label>
             </div>
             <div>
                <label>
                   <span>Email address <span className="required">*</span></span>
-                  <input type="email" />
+                  <input type="email" onChange={handleInputChange} name='email' />
                </label>
             </div>
             <div>
                <label>
                   <span>Password <span className="required">*</span></span>
-                  <input type="password" />
+                  <input type="password" onChange={handleInputChange} name='password' />
                </label>
             </div>
             <div className="privacy-policy-text remember">
