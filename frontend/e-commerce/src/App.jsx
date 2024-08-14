3a9e4f858a0279a902e2f6f4d432e5572955d0b7
zeynepfahriye <fahriyezeynepcakir@gmail.com>
2024-08-14 04:25:52 +0300

    admin user page

--- frontend/e-commerce/src/App.jsx
+++ frontend/e-commerce/src/App.jsx
@@ -6,6 +6,8 @@ import BlogPage from './pages/BlogPage';
 import ContactPage from './pages/ContactPage';
 import CartPage from './pages/CartPage';
 import AuthPage from './pages/AuthPage';
+import ProductsDetailPage from './pages/ProductsDetailPage';
+import AdminUserPage from './pages/admin/AdminUserPage';
 import './App.css'
 
 function App() {
@@ -14,10 +16,14 @@ function App() {
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/shop' element={<ShopPage />} />
-      <Route path='/blog' element={<BlogPage />} />
       <Route path='/contact' element={<ContactPage />} />
       <Route path='/cart' element={<CartPage />} />
       <Route path='/auth' element={<AuthPage />} />
+      <Route path='/product/:id' element={<ProductsDetailPage />} />
+      <Route path='/blog/id' element={<BlogPage />} />
+      <Route path='/admin/*'>
+        <Route path='users' element={<AdminUserPage />} />
+      </Route>
     </Routes>
   )
 }
